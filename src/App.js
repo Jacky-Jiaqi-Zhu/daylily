858057deee4f87443ba9201eeaf999bd73b025f0
Jiaqi Zhu <jiaqi.zhu.jacky@gmail.com>
2019-08-10 19:58:54 +1000

    devia original

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { Component } from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Chart } from 'react-chartjs-2';
+import { ThemeProvider } from '@material-ui/styles';
+import validate from 'validate.js';
 
-function App() {
+import { chartjs } from './helpers';
+import theme from './theme';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+import validators from './common/validators';
+import Routes from './Routes';
+
+const browserHistory = createBrowserHistory();
+
+Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+  draw: chartjs.draw
+});
+
+validate.validators = {
+  ...validate.validators,
+  ...validators
+};
+
+export default class App extends Component {
+  render() {
     return (
-        <React.Fragment>
-            <CssBaseline />
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-        </React.Fragment>
-
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <Routes />
+        </Router>
+      </ThemeProvider>
     );
+  }
 }
-
-export default App;
